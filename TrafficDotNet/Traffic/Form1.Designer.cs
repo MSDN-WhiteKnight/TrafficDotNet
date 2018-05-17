59743aac348dc0fef880be17ce10f4ea9f413680
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2018-05-17 23:41:11 +0500

    Add files via upload

---
+++ TrafficDotNet/Traffic/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Traffic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartAll = new System.Windows.Forms.Button();
+            this.btnTransport = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(441, 385);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(358, 79);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(251, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(97, 28);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(441, 53);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(115, 28);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(24, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(390, 404);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid1.LineColor = System.Drawing.SystemColors.ControlDark;
+            this.propertyGrid1.Location = new System.Drawing.Point(441, 120);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(358, 240);
+            this.propertyGrid1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(605, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ".";
+            // 
+            // btnStartAll
+            // 
+            this.btnStartAll.Location = new System.Drawing.Point(368, 12);
+            this.btnStartAll.Name = "btnStartAll";
+            this.btnStartAll.Size = new System.Drawing.Size(97, 28);
+            this.btnStartAll.TabIndex = 2;
+            this.btnStartAll.Text = "StartAll";
+            this.btnStartAll.UseVisualStyleBackColor = true;
+            this.btnStartAll.Click += new System.EventHandler(this.btnStartAll_Click);
+            // 
+            // btnTransport
+            // 
+            this.btnTransport.Location = new System.Drawing.Point(490, 11);
+            this.btnTransport.Name = "btnTransport";
+            this.btnTransport.Size = new System.Drawing.Size(109, 28);
+            this.btnTransport.TabIndex = 7;
+            this.btnTransport.Text = "Transport";
+            this.btnTransport.UseVisualStyleBackColor = true;
+            this.btnTransport.Click += new System.EventHandler(this.btnTransport_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(571, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 496);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTransport);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStartAll);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Network Capture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartAll;
+        private System.Windows.Forms.Button btnTransport;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TrafficDotNet/Traffic/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Threading;
+using TrafficLib;
+
+namespace Traffic
+{
+    public partial class Form1 : Form
+    {
+        INetworkEvents session;
+        NetworkEvent packet;
+        NetworkCounter counter;
+
+        INetworkEvents session2;
+        NetworkCounter counter2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //MessageBox.Show(System.Runtime.InteropServices.Marshal.SizeOf(typeof(IntPtr)).ToString());
+
+            var ifs = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (var x in ifs)
+            {
+                if (x.GetIPProperties() == null) continue;
+                if (x.GetIPProperties().UnicastAddresses == null) continue;
+
+                foreach (var y in x.GetIPProperties().UnicastAddresses)
+                {
+                    if (y.Address.AddressFamily != AddressFamily.InterNetwork) continue;
+                    comboBox1.Items.Add(y.Address.ToString());
+                    break;
+                }     
+            }
+
+            if (comboBox1.Items.Count > 0) comboBox1.SelectedIndex = 0;
+
+            
+            
+        }
+                
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            listBox1.Items.Clear();
+
+            var packets = session.Events;
+            foreach (var p in packets)
+            {
+                listBox1.Items.Add(p.ToString());
+            }           
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (session != null)
+            {
+                session.NewEvent -= OnNewPacket;
+                session.End();
+            }
+
+            
+            var ses = new Ip4CaptureSession(IPAddress.Parse(comboBox1.SelectedItem.ToString()));
+            ses.NewEvent += OnNewPacket;
+            ses.Start();
+            this.session = ses;
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            session.NewEvent -= OnNewPacket;
+            session.End();
+
+            if (session2 != null)
+            {
+                session2.NewEvent -= OnNewPacket;
+                session2.End();
+                session2 = null;
+                counter2 = null;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex < 0) return;
+            packet = session.GetEvent((uint)listBox1.SelectedIndex);
+            propertyGrid1.SelectedObject = packet;
+
+            if (packet is Ip4Packet)
+            {
+
+                if ((packet as Ip4Packet).Data != null)
+                {
+                    byte[] data = (packet as Ip4Packet).Data;
+                    StringBuilder sb = new StringBuilder(500);
+
+                    foreach (byte b in data)
+                    {
+                        if ((b >= 32 && b <= 123) || b == 10 || b == 13)
+                        {
+                            char c = (char)b;
+                            sb.Append(c);
+                        }
+                    }
+                    textBox1.Text = sb.ToString();
+                }
+                else
+                    textBox1.Text = packet.ErrorData.ToString();
+            }
+        }
+
+        void OnNewPacket(object sender, NetworkEvent e)
+        {
+            this.label1.BeginInvoke((MethodInvoker)(
+                delegate {
+
+                    if (this.counter != null)
+                    {
+                        this.label1.Text = "Recv: " + this.counter.RecvBytes.ToString() +
+                            " Send: " + this.counter.SentBytes.ToString()+
+                            " Total: " + this.counter.TotalBytes.ToString();
+
+                    }
+                    else this.label1.Text = e.TotalLength.ToString();
+
+                    if (this.counter2 != null)
+                    {
+                        this.label2.Text = "Recv: " + this.counter2.RecvBytes.ToString() +
+                            " Send: " + this.counter2.SentBytes.ToString() +
+                            " Total: " + this.counter2.TotalBytes.ToString();
+                    }
+                    else label2.Text = "";
+
+                    /*this.label1.Text += "."; 
+                    if(this.label1.Text.Length > 10)this.label1.Text=".";*/
+                    
+                }));
+        }
+
+        private void btnStartAll_Click(object sender, EventArgs e)
+        {
+            var st = new NetworkStats();
+            st.NewEvent += this.OnNewPacket;
+
+            //this.counter = new SpecificAddressCounter(IPAddress.Parse("89.108.91.180"));
+            this.counter = new AllTrafficCounter();
+            st.AddCounter(this.counter);
+
+            st.Start();
+            this.session = st;
+
+            //********
+            var events = NetworkStats.TransportLayerStats;
+            events.NewEvent += this.OnNewPacket;
+                        
+            this.counter2 = new AllTrafficCounter();
+            //this.counter2 = new SpecificAddressCounter(IPAddress.Parse("89.108.91.180"));
+            events.AddCounter(this.counter2);
+            events.Start();
+            this.session2 = events;
+        }
+
+        private void btnTransport_Click(object sender, EventArgs e)
+        {
+            var events = NetworkStats.TransportLayerStats;
+            events.NewEvent += this.OnNewPacket;
+
+            //this.counter = new SpecificAddressCounter(IPAddress.Parse("89.108.91.180"));
+            this.counter = new AllTrafficCounter();
+            events.AddCounter(this.counter);  
+            ;
+
+            events.Start();
+            this.session = events;
+        }
+    }
+}
